f4112706332d0c2d0e87b5397a364278df9474d2
Mr.Bot <jens@atecmedia.com>
2025-05-03 11:43:22 +0200

    Update build-and-run.sh

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,6 +1,5 @@
 username=$(whoami)
 compilerPath="/Users/$username/Developer/PlaydateSDK/bin/pdc"
-simulatorPath="/Users/$username/Developer/PlaydateSDK/bin/Playdate Simulator.app/Contents/MacOS/Playdate Simulator"
 sourcePath="./source"
 outputPath="./builds/"
 outputFile="$(basename "$(pwd)").pdx"
@@ -11,7 +10,6 @@ if [ ! -d "$outputPath" ]; then
 fi
 
 echo "Compiler path:  $compilerPath"
-echo "Simulator path: $simulatorPath"
 echo "Source path:    $sourcePath"
 echo "Output path:    $outputPath$outputFile"
 echo ""
@@ -22,4 +20,4 @@ echo "Compile source..."
 "$compilerPath" -k "$sourcePath" "$outputPath$outputFile"
 
 echo "Opening simulator..."
-"$simulatorPath" "$outputPath$outputFile"
+open "$outputPath$outputFile"
