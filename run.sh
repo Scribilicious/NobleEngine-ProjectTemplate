d81f2f5a228403cac7be7d2fcf071b94c48e75f4
Mr.Bot <jens@atecmedia.com>
2025-05-03 11:42:52 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
-simulatorPath='/Users/johannes.eldering/Developer/PlaydateSDK/bin/Playdate Simulator.app/Contents/MacOS/Playdate Simulator'
 outputPath="./builds/"
 outputFile="$(basename "$(pwd)").pdx"
 
-echo "Simulator path: $simulatorPath"
-echo "Output path:    $outputPath$outputFile"
+echo "Output path: $outputPath$outputFile"
 echo ""
 
 echo "Opening simulator..."
-"$simulatorPath" "$outputPath$outputFile"
+open "$outputPath$outputFile"
